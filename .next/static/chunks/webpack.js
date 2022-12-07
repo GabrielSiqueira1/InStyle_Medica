f103c415015fadc120903706caad2f754459a50e
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2022-12-07 15:03:25 -0300

    Formulario 4 pagina

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "178efe5ad3226d8a"; }
+/******/ 		__webpack_require__.h = function() { return "38b3e7970fe9781b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- scheduler/routes/index.js
+++ scheduler/routes/index.js
@@ -6,15 +6,39 @@ var database = require('../database');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  database.query('SELECT DISTINCT especialidade FROM agendamento ORDER BY especialidade ASC', 
+  database.query('SELECT DISTINCT especialidade FROM medicos ORDER BY especialidade ASC', 
   function(error, data){
     res.render('index', { title: 'Express' , especialidade_data : data});
   });
 
 });
 
-router.get("/add", function(request, response, next){
+router.get("/", function(request, response, next){
   response.render('index', {title: 'Inserindo na agenda do MySQL', action:'add'});
-})
+});
+
+router.post("/add", function(request, response, next){
+  var especialidade = request.body.especialidade;
+  var medico = request.body.medico;
+  var data = request.body.exame;
+  var horario = request.body.horario;
+  var nome = request.body.nome;
+  var telefone = request.body.telefone;
+  var email = request.body.email;
+
+  var query = `
+    INSERT INTO agenda (especialidade, medico, data, horario, paciente, telefone, email) VALUES 
+    ("${especialidade}", "${medico}", "${data}", "${horario}", "${nome}", "${telefone}", "${email}")
+  `;
+
+  database.query(query, function(error, data){
+    if(error){
+      throw error;
+    }else{
+      response.redirect("localhost:3000");
+    }
+  });
+
+});
 
 module.exports = router;
